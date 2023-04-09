37bcf7d7abcf0730fae15ce4db39c744e57c5ba2
Anthony Iszer <aiszler29@gmail.com>
2023-04-08 23:46:33 -0700

    added comments / fixed number

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,14 +1,14 @@
 def fizzbuzz():
      for i in range(1,100):
         if i % 2 == 0:
-                print("fizz")
-
+                print("fizz")  #prints fizz if divisible by 2
         if i % 3 == 0:
-                print("buzz")
-        if i % 3 == 0 & i % 3 == 0:
-             print("fizzbuzz")
+                print("buzz")   #prints buzz if divisible by 3
+        if i % 3 == 0 & i % 2 == 0:
+             print("fizzbuzz")  #prints fizzbuzz if divisible by 3 and 2
         else:
-             print(i)     
-
+             print(i)     #prints the rest of the numbers
 fizzbuzz()
+
+#done in function because I wanted to pratice my functions
 
